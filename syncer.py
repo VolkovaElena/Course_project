b83dfbff40843b44af4bedbc1d98b367aa8a3909
Paul Rohochevski <paul.rohochevski@gmail.com>
2017-03-27 14:35:09 +0300

    The directory created function was successfully tested

--- syncer.py
+++ syncer.py
@@ -4,13 +4,13 @@
 Our course project.
 """
 
-import argparse, subprocess, sys, os
+import argparse, subprocess, sys, os, pipes
 
 
 # Парсит вводимые аргументы
 def super_funk():
     parser = argparse.ArgumentParser(
-        description="[keys] [password] [local directory or file name] [destination(root@host)]")
+        description="[keys] [password] [local directory or file name] [destination(root@host:)]")
     parser.add_argument("-P", action="store_const", help="--partial--progress", const="P")
     parser.add_argument("-v", "--verbose", action="store_const", help="increase output verbosity", const="v")
     parser.add_argument("-e", nargs=1, help="specify the remote shell to use")
@@ -67,8 +67,11 @@ def parse_ip():
     ip_str = source_dst()[1]
     index_split = ip_str.index("@")
     middle_ip_str = ip_str[index_split + 1:]
-    index_final_split = middle_ip_str.index(":")
-    final_ip_str = middle_ip_str[:index_final_split]
+    if ":" in middle_ip_str:
+        index_final_split = middle_ip_str.index(":")
+        final_ip_str = middle_ip_str[:index_final_split]
+    else:
+        final_ip_str = middle_ip_str
     return final_ip_str
 
 
@@ -83,10 +86,42 @@ def check_ping(hostname):
         sys.exit(1)
 
 
+# Destination for check_dir func
+def parse_dir():
+    dir_str = source_dst()[1]
+    index_split = dir_str.index("@")
+    middle_dir_str = dir_str[index_split + 1:]
+    if ":" in middle_dir_str:
+        index_final_split = middle_dir_str.index(":")
+        final_dir_str = middle_dir_str[index_final_split + 1:]
+    else:
+        extra_args = super_funk()["extra_args"]
+        final_dir_str = extra_args[0]
+    return final_dir_str
+
+# Host for check_dir func
+def parse_host():
+    dir_str = source_dst()[1]
+    if ":" in dir_str:
+        index_final_split = dir_str.index(":")
+        final_host = dir_str[:index_final_split]
+    else:
+        final_host = dir_str
+    return final_host
+
+# Check dir
+def is_dir_present(path, host):
+    response = subprocess.call(['ssh', host, 'test -d ' + pipes.quote(path)])
+    return response
+
+
+# Main func
+def remote_dir(path, host):
+    if is_dir_present(path, host) == 1:
+        subprocess.call(['ssh', host, 'mkdir -p ' + pipes.quote(path)])
+
+
 # При запуске из консоли выполняет следующие функции
 if __name__ == "__main__":
-    super_funk()
-    concat_keys()
-    source_dst()
-    parse_ip()
     check_ping(parse_ip())
+    remote_dir(parse_dir(), parse_host())
